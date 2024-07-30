8f0fb913e3a06a9ff47e2a84dab056cecc832b92
TakaTanabe <117871583+Ttnabe@users.noreply.github.com>
2024-07-30 18:04:20 +0900

    Update pcq_template_v2.cmd

--- pcq_template_v2.cmd
+++ pcq_template_v2.cmd
@@ -1,13 +1,13 @@
 Number of uncertainty variables 2
 
 ## Input parameter ranges
-PARAMETER x1-min 0.2
-PARAMETER x1-max 0.5
-PARAMETER x2-min 0.5
-PARAMETER x2-max 6.5
+PARAMETER x1-min 0.5
+PARAMETER x1-max 4.5
+PARAMETER x2-min 0.2
+PARAMETER x2-max 0.5
 
 ## Parameters for PCQ
-## Number of quadrature points indicates that for each parameter
+## "NUMBER OF QUADRATURE POINTS" & "NUMBER OF SECONDARY SAMPLING POINTs" indicate N_Q & N_SSP, respectively
 MAXIMUM DEGREE OF EXPANSION 10
 NUMBER OF QUADRATURE POINTS 10
 NUMBER OF SECONDARY SAMPLING POINTs 100
@@ -15,5 +15,6 @@ NUMBER OF SECONDARY SAMPLING POINTs 100
 ## Threshold value
 cri 0.5
 
-## Set path for output asc file (numerical result)
+## Path for output asc file (numerical result) & save folder
 ./RUN_RES/
+./PCQ_RES/
