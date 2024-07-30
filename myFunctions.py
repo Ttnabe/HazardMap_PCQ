df126f0f64208022016f692ca82e1caab6d753f4
TakaTanabe <117871583+Ttnabe@users.noreply.github.com>
2024-07-30 18:03:24 +0900

    myFunctions.py

--- myFunctions.py
+++ myFunctions.py
@@ -4,31 +4,34 @@ Created on Oct. 12th, 2023, by Takahiro Tanabe
 """
 import os
 import sys
+from scipy import special
 import numpy as np
 
-# Read command file & return the parameters written in the file
-## input : str_file - name of command file
-##         N_val - number of uncertainty input variables
-## output: array of parameters written in command file
+# Read command file & return the parameters list
+## input : str_file - command file name
+## output: list of parameters written in command file
 def read_cmd(str_file):
     cmd_list = []
-    ## Set parameter range & number of nodes for the quadature
+    ## Set parameter ranges & number of nodes for the quadature
+    ## Read command file
     with open(str_file) as f:
         l = f.readlines()
+        ## Number of uncertain variables
         N_val = int(l[0].split()[-1])
         cmd_list += [N_val]
-        ### Minimum & maximum for limit of parameter interval
+        ### Minimum & maximum bounds of each parameter
         for n in range(N_val):
             cmd_list += [float(l[3+2*n].split()[-1])]
             cmd_list += [float(l[3+(2*n+1)].split()[-1])]
 
         ### N_P, N_Q, N_SSP
-        for n in range(6+2*N_val, 6+(2*N_val+3)):
+        for n in range(6+2*N_val, (6+2*N_val)+3):
             cmd_list += [int(l[n].split()[-1])]
 
-        ### h_cri, path for output asc file (numerical result)
+        ### Threshold, folder path for output asc file (numerical result) & folder path for save
         cmd_list += [float(l[11+2*N_val].split()[-1])]
         cmd_list += [l[14+2*N_val][:-1]]
+        cmd_list += [l[15+2*N_val][:-1]]
     return cmd_list
 
 
@@ -46,3 +49,10 @@ def asc_header(str_file):
             else:
                 header_dict[blocks[0]] = round(float(blocks[-1]), 2)
     return header_dict
+
+
+##  Calculate Le_n(x)
+##  input : n - degree of Legendre polynomial
+##          x - A variable in [-1, 1]
+def legendre(n, x):
+    return special.eval_legendre(n, x)
